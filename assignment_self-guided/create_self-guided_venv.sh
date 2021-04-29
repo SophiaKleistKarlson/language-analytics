a0b8eb3b0add600ffa7897518e4ec2fa7738a4d8
Sophia Kleist Karlson <54801990+SophiaKleistKarlson@users.noreply.github.com>
2021-04-29 13:44:12 +0200

    Update create_self-guided_venv.sh

--- assignment_self-guided/create_self-guided_venv.sh
+++ assignment_self-guided/create_self-guided_venv.sh
@@ -1,1 +1,24 @@
+#!/usr/bin/env bash
 
+#first write this:
+#cd cds-language/src
+
+VENVNAME=headline_sentiment_environment
+
+python3 -m venv $VENVNAME
+source $VENVNAME/bin/activate
+pip install --upgrade pip
+
+pip install ipython
+pip install jupyter
+python -m ipykernel install --user --name=$VENVNAME
+
+test -f requirements.txt && pip install -r requirements.txt
+python -m spacy download en_core_web_sm
+
+# then this: 
+python3 keyword_headline_sentiment.py
+
+#then deactivate
+deactivate
+echo "build $VENVNAME"
