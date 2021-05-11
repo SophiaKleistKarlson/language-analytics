b8d73e9e27a8e718ea904bf7e956872a7d109d7c
Sophia Kleist Karlson <54801990+SophiaKleistKarlson@users.noreply.github.com>
2021-05-11 10:39:40 +0200

    Update create_collocation_venv.sh

--- assignment_2/create_collocation_venv.sh
+++ assignment_2/create_collocation_venv.sh
@@ -1,1 +1,21 @@
+#!/usr/bin/env bash
 
+#first set the working directory 
+#cd cds-language
+
+VENVNAME=collocation_environment
+
+python3 -m venv $VENVNAME
+source $VENVNAME/bin/activate
+pip install --upgrade pip
+
+pip install ipython
+pip install jupyter
+python -m ipykernel install --user --name=$VENVNAME
+
+# check and install requirements
+test -f requirements.txt && pip install -r requirements.txt
+
+#then deactivate the environment
+deactivate
+echo "build $VENVNAME"
