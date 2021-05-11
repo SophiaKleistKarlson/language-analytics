9a2ae5173bd1503183dad15580de5c25d2bb4fb1
Sophia Kleist Karlson <54801990+SophiaKleistKarlson@users.noreply.github.com>
2021-05-11 10:37:12 +0200

    Update create_sentiment_venv.sh

--- assignment_3/create_sentiment_venv.sh
+++ assignment_3/create_sentiment_venv.sh
@@ -1,1 +1,21 @@
+#!/usr/bin/env bash
 
+#first set the working directory 
+#cd cds-language/src
+
+VENVNAME=sentiment_environment
+
+python3 -m venv $VENVNAME
+source $VENVNAME/bin/activate
+pip install --upgrade pip
+
+pip install ipython
+pip install jupyter
+python -m ipykernel install --user --name=$VENVNAME
+
+test -f requirements.txt && pip install -r requirements.txt
+python -m spacy download en_core_web_sm
+
+#then deactivate the environment
+deactivate
+echo "build $VENVNAME"
