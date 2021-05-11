de5bf409d7542982e0bbc602f0ec3402c4e1d8b4
Sophia Kleist Karlson <54801990+SophiaKleistKarlson@users.noreply.github.com>
2021-05-11 09:59:18 +0200

    Update create_keyword_sentiment_venv.sh

--- assignment_self-guided/create_keyword_sentiment_venv.sh
+++ assignment_self-guided/create_keyword_sentiment_venv.sh
@@ -17,7 +17,7 @@ test -f requirements.txt && pip install -r requirements.txt
 python -m spacy download en_core_web_sm
 
 # then run the script: (in worker2 , it wouldn't be in the "src" folder, because the requirements and scripts are in the same folder)
-python3 src/keyword_headline_sentiment.py 
+#python3 src/keyword_headline_sentiment.py 
 
 #then deactivate the environment
 deactivate
